5715d4affdaac689917acf901df105897a7addf7
umadayal <uma.dayal@yahoo.com>
2024-10-02 18:40:10 -0700

    cleanup

--- generate_regex_bin/src/main.rs
+++ generate_regex_bin/src/main.rs
@@ -7,25 +7,8 @@ use std::io::Write;
 // and saves them into binary files.
 fn main() {
     let re = Regex::new(r"This email was meant for (@\w+)").unwrap();
-    // #[repr(C)]
-    // struct Aligned<B: ?Sized> {
-    //     _align: [u32; 0],
-    //     bytes: B,
-    // }
-    // let mut buf_fwd = Aligned { _align: [], bytes: [0u8; 4 * (1<<10)] };
-    // let mut buf_rev = Aligned { _align: [], bytes: [0u8; 4 * (1<<10)] };
-    // let written_fwd = re.forward().write_to_native_endian(&mut buf_fwd.bytes).expect("Failed to write DFA to buffer");
-    // let written_rev = re.reverse().write_to_native_endian(&mut buf_rev.bytes).expect("Failed to write DFA to buffer");
-    // let mut file = File::create("dfa_fwd_bytes.bin").unwrap();
-    // file.write_all(&buf_fwd.bytes[..written_fwd]).unwrap();
-    // let mut file = File::create("dfa_rev_bytes.bin").unwrap();
-    // file.write_all(&buf_rev.bytes[..written_rev]).unwrap();
     let (fwd_bytes, fwd_pad) = re.forward().to_bytes_little_endian();
     let (rev_bytes, rev_pad) = re.reverse().to_bytes_little_endian();
-    // let mut file = File::create("dfa_fwd_bytes.bin").unwrap();
-    // file.write_all(&fwd_bytes[fwd_pad..]).unwrap();
-    // let mut file = File::create("dfa_rev_bytes.bin").unwrap();
-    // file.write_all(&rev_bytes[rev_pad..]).unwrap();
     std::fs::write("dfa_fwd_bytes.bin", &fwd_bytes[fwd_pad..]).unwrap();
     std::fs::write("dfa_rev_bytes.bin", &rev_bytes[rev_pad..]).unwrap();
 }
